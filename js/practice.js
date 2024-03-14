83b5fc0331ca984886485f3511055dafce3ab50e
WhyBusyy <ybg6152@naver.com>
2024-03-14 17:52:35 +0900

    추가

---
+++ js/practice.js
@@ -0,0 +1,24 @@
+let mainSlide = new Swiper(".mainSlide", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+  pagination: {
+    el: ".pg1",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+let eduSlide = new Swiper(".eduSlide", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".eduPage",
+    clickable: true,
+  },
+});
