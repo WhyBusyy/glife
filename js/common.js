493864b72859151d48b4188eb8ddd3bbf2ce5c37
WhyBusyy <ybg6152@naver.com>
2024-03-18 13:34:33 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,9 +1,25 @@
-var swiper = new Swiper(".mv_slide", {
+let mainSlide = new Swiper(".mainSlide", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".pg1",
     clickable: true,
     renderBullet: function (index, className) {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
+  },
+});
+
+let eduSlide = new Swiper(".eduSlide", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 32,
+  loop: true,
+  autoplay: {
+    delay: 3000,
   },
 });
